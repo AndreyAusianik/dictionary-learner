c5b525ec9aab25839f763456470ae9375a750890
Andrei Ausianik <andrei.ausianik@iii.com>
2017-10-23 17:44:37 +0300

    dictionary learner solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function findRotationPoint(words) {
-  // your solution here
-}
+    let left = 0,
+        right = words.length - 1,
+        middle = (right + left) / 2 | 0;
+    while(right - left > 1) {
+        if(words[left] > words[middle]) {
+            right = middle;
+        } else if(words[right] < words[middle]) {
+            left = middle;
+        } else {
+            return left;
+        }
+        middle = (right + left) / 2 | 0;
+    }
+    return right;
+};
